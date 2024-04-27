b0b603864736e976a891c17081beaf62347964f5
Ashwin Shaharkar <ashwin.shaharkar@gmail.com>
2024-04-27 20:43:06 +0530

    Fixed error.

--- src/app/question/[id]/page.js
+++ src/app/question/[id]/page.js
@@ -3,7 +3,7 @@ import { promises as fs } from 'fs';
 
 
 export default async function Questions({ params }) {
-  const file = await fs.readFile(process.cwd() + '/src/app/data.json', 'utf8');
+  const file = await fs.readFile(process.cwd() + '/data.json', 'utf8');
   const data = JSON.parse(file);
   const id = params.id;
 
--- src/app/question/page.js
+++ src/app/question/page.js
@@ -3,13 +3,14 @@ import { promises as fs } from 'fs';
 
 
 export default async function Questions({ params }) {
-  const file = await fs.readFile(process.cwd() + '/src/app/data.json', 'utf8');
+  const file = await fs.readFile(process.cwd() + '/data.json', 'utf8');
   const data = JSON.parse(file);
   const id = 1;
+
   if (params.id) {
     const id = params.id;
   }
-  
+
   return (
     <div className="container">
       <h3 className="h3">{data[id].title}</h3>
